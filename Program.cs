f61eb71a30badcb91c397c59f0e0004d2b3f08bd
exoil <jakubtederko@outlook.com>
2025-02-16 13:32:29 +0100

    Update

--- Program.cs
+++ Program.cs
@@ -1,11 +1,15 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ProjectAssistant;
+using ProjectAssistant.IoC;
 using Serilog;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddMediatR(configuration =>
+    configuration.RegisterServicesFromAssembly(typeof(RegisterLlmClients).Assembly));
+builder.Services.RegisterOpenAiClient();
 Log.Logger = new LoggerConfiguration()
     .WriteTo.BrowserConsole()
     .CreateLogger();
